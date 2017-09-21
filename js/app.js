69beaefe9cf3a16d7d0fda50ca667b521ff435c6
unknown <fer0309g@gmail,com>
2017-09-21 16:10:23 -0700

    Loving JS!

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 var firstName = prompt("What is your first name?");
 var lastName = prompt("What is your last name?");
-{console.log(firstName);
+
+console.log(firstName);
 alert(lastName);
+
+{
+  var dateString = prompt("When were you born? (MM/DD/YYYY)");
+  var birthDate = new Date(dateString);
+  var today = new Date();
+  var age = today.getFullYear() - birthDate.getFullYear();
+  alert("You're " + age + " this year. :D");
 }
--- js/script.js
+++ js/script.js
@@ -1,1 +1,1 @@
-console.log("I'd like to be able to learn javascript from this class to apply it to my skillset to become a fullstack dev.");
+//console.log("I'd like to be able to learn javascript from this class to apply it to my skillset to become a fullstack dev.");
